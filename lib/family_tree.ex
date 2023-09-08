b38d9df277e9cc99f7328d0d4f8f5b9ce7daf425
Maheep Kumar Kathuria <me@maheepk.net>
2023-09-08 16:50:41 +0530

    Added support for specializing a relation entity
    Added helper functions to specialize/alias a relation entity. Some examples:
    - if a person in role of parent is a male, then parent should be father.
    - if a person in role of child is a female, then child should be daughter.
    
    Signed-off-by: Maheep Kumar Kathuria <me@maheepk.net>

--- lib/family_tree.ex
+++ lib/family_tree.ex
@@ -63,6 +63,55 @@ defmodule FamilyTree do
   end
 
   @doc """
+  Sets alias of a given relationship for given conditions.
+
+    ## Examples
+
+      iex> FamilyTree.set_relationship("parent", "father", [gender: "male"])
+      :ok
+
+  """
+  def set_relationship(relationship, relationship_alias, conditions_list) do
+    {:ok, table} = :dets.open_file(:relationship_alias_vocabulary, [type: :set])
+    try do
+      with :ok <- :dets.insert(table, {{relationship, relationship_alias}, conditions_list |> Enum.into(%{})}),
+         :ok <- :dets.insert(table, {{relationship_alias, relationship}, conditions_list |> Enum.into(%{})}) do
+        :ok
+      end
+    after
+      :dets.close(table)
+    end
+  end
+
+  @doc """
+  Given a relationship or its alias, return the main relationship and the conditions that satisfy the alias
+
+    ## Examples
+
+      iex> FamilyTree.set_relationship("parent", "father", [gender: "male"])
+      :ok
+      iex> FamilyTree.get_relationship_and_cond_for_alias("father")
+      {:ok, "parent", %{gender: "male"}}
+
+  """
+  def get_relationship_and_cond_for_alias(relationship_or_alias) do
+    {:ok, table} = :dets.open_file(:relationship_alias_vocabulary, [type: :set])
+    try do
+      case get_complementary_relation_of(relationship_or_alias) do
+        {:ok, _} -> {:ok, relationship_or_alias, %{}}
+        _ ->
+          with [{relationship_or_alias_2, conditions}] <- :dets.select(table, [{{{:"$1", relationship_or_alias}, :"$2"}, [], [{{:"$1", :"$2"}}]}]) do
+            {:ok, relationship_or_alias_2, conditions}
+          else
+            _ -> {:error, "Relation not defined."}
+          end
+      end
+    after
+      :dets.close(table)
+    end
+  end
+
+  @doc """
   Get complementary relation of the given relationship.
 
   Relationships can be:
