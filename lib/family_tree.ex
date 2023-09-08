1ad1ae3038350164dc8fce7345ec17edc4d8dbdf
Maheep Kumar Kathuria <me@maheepk.net>
2023-09-08 16:46:38 +0530

    Added functions to add and read a relation definition
    Added the following functions:
    - add_relationship
    - get_complementary_relation_of
    
    Signed-off-by: Maheep Kumar Kathuria <me@maheepk.net>

--- lib/family_tree.ex
+++ lib/family_tree.ex
@@ -36,4 +36,59 @@ defmodule FamilyTree do
       :dets.close(table)
     end
   end
+
+  @doc """
+  Define a relationship in the relationship dictionary.
+
+  Relationships can be:
+  - complementary of each other, for e.g. Parent-Child (If x is Parent of y, then y is Child of x)
+  - self-complementary, for e.g. Sibling (If x is Sibling of y, then y is Sibling of x)
+
+    ## Examples
+
+      iex> FamilyTree.add_relationship("parent", "child")
+      :ok
+
+  """
+  def add_relationship(relationship1, relationship2) do
+    {:ok, table} = :dets.open_file(:relationship_complements_vocabulary, [type: :set])
+    try do
+      with :ok <- :dets.insert(table, {relationship1,relationship2}),
+         :ok <- :dets.insert(table, {relationship2,relationship1}) do
+        :ok
+      end
+    after
+      :dets.close(table)
+    end
+  end
+
+  @doc """
+  Get complementary relation of the given relationship.
+
+  Relationships can be:
+  - complementary of each other, for e.g. Parent-Child (If x is Parent of y, then y is Child of x)
+  - self-complementary, for e.g. Sibling (If x is Sibling of y, then y is Sibling of x)
+
+    ## Examples
+
+      iex> FamilyTree.add_relationship("parent", "child")
+      :ok
+      iex> FamilyTree.get_complementary_relation_of("parent")
+      {:ok, "child"}
+      iex> FamilyTree.get_complementary_relation_of("child")
+      {:ok, "parent"}
+
+  """
+  def get_complementary_relation_of(relationship) do
+    {:ok, table} = :dets.open_file(:relationship_complements_vocabulary, [type: :set])
+    try do
+      # :ets.fun2ms(fn {relation, complement} when relation==relationship -> complement end)
+      result = with [complement] <- :dets.select(table, [{{:"$1", :"$2"}, [{:==, :"$1", relationship}], [:"$2"]}]) do
+        {:ok, complement}
+      end
+      if result != [] and result != nil, do: result, else: {:error, "Complement relation for #{relationship} not found."}
+    after
+      :dets.close(table)
+    end
+  end
 end
